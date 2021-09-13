d318f419cadce2c74ed779932ec94c7a03f08c26
DuhnovOleg <ol.duhnov@yandex.ru>
2021-09-13 22:46:13 +0300

    report

--- Лабораторная работа 1/main.c
+++ Лабораторная работа 1/main.c
@@ -6,86 +6,90 @@
 #include <string.h>
 #include <conio.h>
 
-/*int main()
-{
-	setlocale(LC_ALL, "Ru");
-	int arr[10] = {123, -63, 22, 9, 0, 24, -2, 923, 1000, -19}, max = INT_MIN, min = INT_MAX;
+//int main()
+//{
+//	setlocale(LC_ALL, "Ru");
+//	int arr[10] = {123, -63, 22, 9, 0, 24, -2, 923, 1000, -19}, max = INT_MIN, min = INT_MAX;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (arr[i] < min)
+//			min = arr[i];
+//
+//		if (arr[i]> max)
+//			max = arr[i];
+//	}
+//	printf("Разница: %d", max - min);
+//	_getch();
+//}
 
-	for (int i = 0; i < 10; i++)
-	{
-		if (arr[i] < min)
-			min = arr[i];
+//int main()
+//{
+//	setlocale(LC_ALL, "Ru");
+//	srand(time(NULL));
+//	int arr[10];
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		arr[i] = rand() % 201 - 101;
+//		printf("%d ", arr[i]);
+//	}
+//	_getch();
+//}
 
-		if (arr[i]> max)
-			max = arr[i];
-	}
-	printf("Разница: %d", max - min);
-}*/
+//int main()
+//{
+//	setlocale(LC_ALL, "Ru");
+//	int* arr, i, n;
+//
+//	printf("Введите размер массива: ");
+//	scanf("%d", &n);
+//	arr = (int*)malloc(n * sizeof(int));
+//	
+//	printf("\nЗаполните массив:\n");
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//
+//	printf("\nРезультат:\n");
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	free(arr);
+//	_getch();
+//}
 
-/*int main()
-{
-	setlocale(LC_ALL, "Ru");
-	srand(time(NULL));
-	int arr[10];
-
-	for (int i = 0; i < 10; i++)
-	{
-		arr[i] = rand() % 201 - 101;
-		printf("%d ", arr[i]);
-	}
-}*/
-
-/*int main()
-{
-	setlocale(LC_ALL, "Ru");
-	int* arr, i, n;
-
-	printf("Введите размер массива: ");
-	scanf("%d", &n);
-	arr = (int*)malloc(n * sizeof(int));
-	
-	printf("\nЗаполните массив:\n");
-	for (i = 0; i < n; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-
-	printf("\nРезультат:\n");
-	for (i = 0; i < n; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	free(arr);
-}*/
-
-/*int main()
-{
-	setlocale(LC_ALL, "Ru");
-	srand(time(NULL));
-	int arr[10][10], i, j, x = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			arr[i][j] = rand() % 201 - 101;
-			printf(" %5d ", arr[i][j]);
-
-		}
-		printf("\n");
-	}
-
-	printf("\n\n");
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			x = x + arr[i][j];
-		}
-		printf("Сумма %d-й строки равна: %3d\n",i + 1, x);
-		x = 0;
-	}
-}*/
+//int main()
+//{
+//	setlocale(LC_ALL, "Ru");
+//	srand(time(NULL));
+//	int arr[10][10], i, j, x = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//		{
+//			arr[i][j] = rand() % 201 - 101;
+//			printf(" %5d ", arr[i][j]);
+//
+//		}
+//		printf("\n");
+//	}
+//
+//	printf("\n\n");
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j < 10; j++)
+//		{
+//			x = x + arr[i][j];
+//		}
+//		printf("Сумма %d-й строки равна: %3d\n",i + 1, x);
+//		x = 0;
+//	}
+//	_getch();
+//}
 
 int main(void)
 {
